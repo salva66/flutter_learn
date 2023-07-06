1f43e6102a7af424dfc9d77e6e4672265e8be74f
ChrissDisigale <chrispro.genius@gmail.com>
2023-07-06 12:42:31 +0300

    build.gradle

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutterapps\vyombo"
+export "FLUTTER_ROOT=/Users/chriss/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/macbookpro/AndroidStudioProjects/flutter_learn"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
